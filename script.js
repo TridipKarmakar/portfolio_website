9720b74c2b8c27703d8cd0bd27478e9eef2a7c08
Tridip Karmakar <karmakartridip97@gmail.com>
2024-03-20 23:05:47 +0530

    1 to 435

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-// JavaScript code
+function scrollToNext() {
+  const sections = document.querySelectorAll('section');
+  let currentSectionIndex = 0;
 
-// Function to toggle the hamburger menu
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top >= 0 && rect.top <= window.innerHeight) {
+      currentSectionIndex = index;
+    }
+  });
+
+  const nextSectionIndex = currentSectionIndex + 1;
+  if (nextSectionIndex < sections.length) {
+    sections[nextSectionIndex].scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-// Function to handle smooth scrolling when clicking on links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
